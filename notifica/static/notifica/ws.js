0a299b4be3a503ec6dfce6b28dbdbc6d0d5ff669
Harry Huy <harryhuydoan@gmail.com>
2017-05-31 20:52:22 +0700

    Working: handler socket closing event

--- notifica/static/notifica/ws.js
+++ notifica/static/notifica/ws.js
@@ -2,3 +2,6 @@ socket = new WebSocket("ws://" + window.location.host + "/");
 socket.onmessage = function(e) {
     console.log(e.data);
 }
+socket.onclose = function(event) {
+    console.log('ws server down');
+}
