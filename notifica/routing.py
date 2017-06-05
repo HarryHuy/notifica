96fe5b3024e3b1008b93390d54b30691499edc89
Harry Huy <harryhuydoan@gmail.com>
2017-06-06 03:25:46 +0700

    Edit: bright imported module name

--- notifica/routing.py
+++ notifica/routing.py
@@ -1,10 +1,10 @@
 from channels.routing import route
-import notifica.consumers as csm
+import notifica.consumers as consumers
 
 channel_routing = [
     # route("http.request", "notifica.consumers.http_consumer"),
-    route('websocket.connect', csm.ws_add),
-    route('websocket.receive', csm.ws_message),
-    route('websocket.disconnect', csm.ws_disconnect),
-    route('notify', csm.ws_manual)
+    route('websocket.connect', consumers.ws_add),
+    route('websocket.receive', consumers.ws_message),
+    route('websocket.disconnect', consumers.ws_disconnect),
+    route('notify', consumers.ws_send_notify)
 ]
