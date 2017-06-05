86b5b44ff9ccfaaa8b71d1306281a2394db632b5
Harry Huy <harryhuydoan@gmail.com>
2017-06-06 03:21:30 +0700

    Edit: user model name

--- notifica/admin.py
+++ notifica/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import *
 
-admin.site.register(CustomUser, UserAdmin)
+admin.site.register(ExtendedUser, UserAdmin)
 admin.site.register(Org)
 admin.site.register(Activity)
 admin.site.register(Notify)
