b9e11fbccbb303273e7775d6195532ab6c855c77
Harry Huy <harryhuydoan@gmail.com>
2017-06-06 03:22:01 +0700

    Remove: test code

--- notifica/consumers.py
+++ notifica/consumers.py
@@ -4,7 +4,9 @@ from channels import Group, Channel
 from channels.auth import channel_session_user, channel_session_user_from_http
 from channels.message import Message
 import gc
+from .base import BaseManager
 
+logged_users = BaseManager('logged_users', 'users')
 
 def http_consumer(message):
     response = HttpResponse("Hello world! You asked for %s" % message.content['path'])
@@ -14,36 +16,31 @@ def http_consumer(message):
 
 @channel_session_user_from_http
 def ws_add(message):
-    # if message.user.is_authenticated is False:
-    #     message.reply_channel.send({'accept': False})
-    # else:
-    #     message.reply_channel.send({'accept': True})
-    #     Group('notify').add(message.reply_channel)
-    message.reply_channel.send({'accept': True, 'text': 'ws connection successful'})
-    Group('notify').add(message.reply_channel)
+    if message.user.is_authenticated is False:
+        message.reply_channel.send({'accept': False})
+    else:
+        message.reply_channel.send({'accept': True, 'text': 'ws connection successful'})
+        Group('notify-%s' % message.user.id).add(message.reply_channel)
+        logged_users.add(message.user)
 
 
 @channel_session_user
 def ws_disconnect(message):
     message.reply_channel.send({'text': 'ws disconnected'})
-    Group('notify').discard(message.reply_channel)
+    Group('notify-%s' % message.user.id).discard(message.reply_channel)
+    logged_users.delete(message.user)
 
 
 @channel_session_user
 def ws_message(message):
-    # print(message.user, message.reply_channel)
     # Group('notify').send({
-        # 'text': '[%s] %s' % (message.user, message['text'])
+    #     'text': '[%s] %s' % (message.user, message['text'])
     # })
     message.reply_channel.send({'text': 'message recieved'})
-    # inst = [obj for obj in gc.get_referrers(Channel) if isinstance(obj, Channel)]
-    # for i in inst:
-        # print(i.__dict__)
-    #     if i.channel.name == 'websocket.connect':
-    #         if i.user.username == 'harry':
-    #             i.reply_channel.send({'text': message.content['text']})
 
 def ws_manual(message):
-    g = Group('notify')
-    g.send({'text': 'radiocheck'})
+    Group('notify').send({'text': 'radiocheck'})
 
+def ws_send_notify(message):
+    if logged_users.get(message.text.user):
+        Group('notyfi-%s' % message.text.user.id).send({'text': message.text.message_content})
