44f220b10ad6c3ee9a8873b496bacb62edfe9891
Harry Huy <harryhuydoan@gmail.com>
2017-06-06 03:29:32 +0700

    Working: change member position procedure

--- notifica/views.py
+++ notifica/views.py
@@ -1,18 +1,47 @@
 from django.shortcuts import render
 from channels import Channel
 from django.http import HttpResponse
-from .base import BaseManager
 import json
-
+from .models import Position, ExtendedUser, Notify
 
 def home(request):
     return render(request, 'home/home.html')
 
-def send_status(request):
+def radio_check(request):
     Channel("notify").send({'text': 'radio-check'})
     return HttpResponse(status=200)
 
-def users_online(request):
-    users = BaseManager('users_logged_in', 'users').add(request.user)
-    dump = json.dumps(users.count())
+def change_member_position(request):
+    if request.method != 'POST':
+        return render(request, 'member_position/update.html')
+    member = ExtendedUser.objects.get(id=request.POST['member_id'])
+    position = Position.objects.get(id=request.POST['position_id'])
+    member.position = position
+    try:
+        member.save()
+    except:
+        return HttpResponse(status=404)
+    n = Notify()
+    creator = ExtendedUser.objects.get(id=request.user.id)
+    recipient = ExtendedUser.objects.get(id=request.POST['member_id'])
+    n.creator = creator
+    n.recipient = recipient
+    n.state = 'unseen'
+    n.type = 'Position changed'
+    n.url = 'user/%s' % request.POST['member_id']
+    try:
+        n.save()
+    except:
+        # print('Notify generation error')
+        raise
+    return HttpResponse(status=200)
+
+def list_user(request):
+    users = ExtendedUser.objects.all()
+    dump = json.dumps([obj for obj in users.values('id', 'username', 'position__name')])
+    return HttpResponse(dump, content_type='application/json')
+
+def user_detail(request, id):
+    user = ExtendedUser.objects.get(id=id)
+    dump = json.dumps([user.username, user.email, user.position.name])
     return HttpResponse(dump, content_type='application/json')
