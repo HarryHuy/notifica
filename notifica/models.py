a62beae1cd503b94ba45f5b6034439b9ff74741c
Harry Huy <harryhuydoan@gmail.com>
2017-06-06 03:23:48 +0700

    Edit: rename fields of user and notify model

--- notifica/models.py
+++ notifica/models.py
@@ -3,20 +3,26 @@ from django.contrib.auth.models import AbstractUser
 from django.conf import settings
 
 
-class CustomUser(AbstractUser):
-    code = models.PositiveIntegerField(null=True)
-    # position = models.ForeignKey(Position)
-
-
 class Position(models.Model):
     name = models.CharField(max_length=30)
-    permission = models.CharField(max_length=30)
-    descrip = models.CharField(max_length=50)
+    # permission = models.CharField(max_length=30)
+    description = models.CharField(max_length=50, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class ExtendedUser(AbstractUser):
+    code = models.PositiveIntegerField(null=True)
+    position = models.ForeignKey(Position, null=True)
+
+    def __str__(self):
+        return self.username
 
 
 class Org(models.Model):
     name = models.CharField(max_length=30)
-    descrip = models.CharField(max_length=50)
+    description = models.CharField(max_length=50)
 
 
 class Activity(models.Model):
@@ -25,14 +31,11 @@ class Activity(models.Model):
     date = models.DateField()
     participate = models.ManyToManyField(settings.AUTH_USER_MODEL)
 
-    def __str__(self):
-        return self.name
-
 
 class Notify(models.Model):
     recipient = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='notify_recipient')
-    sender = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='notify_sender')
+    creator = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='notify_sender')
     state = ('read', 'unread', 'unseen')
     type = models.CharField(max_length=30)
-    reference = models.CharField(max_length=50)
+    url = models.CharField(max_length=50)
 
