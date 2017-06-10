2d7cf48cc0a8b85c3aed9361a53bfc2b21856f33
Harry Huy <harryhuydoan@gmail.com>
2017-06-11 02:43:27 +0700

    Disable: uknown errors

--- notifica/static/notifica/notify_client.js
+++ notifica/static/notifica/notify_client.js
@@ -1,21 +1,27 @@
+socket.onmessage = function(event) {
+//    load_msg(event.text);
+    console.log(event.data);
+}
+
+//var load_msg = function(msg) {
+//    var type = msg['type'];
+//    var creator = msg['creator'];
+//    var url = msg['url'];
+//    $('.dropdown-menu').prepend('<li><a href="'+ url +'">'+ type + ', '+ creator +'</a></li>');
+//}
+//
+//var delete_msg = function(msg) {
+//    socket.send('delete'+ msg);
+//}
+
 $('document').ready(function() {
-    recieve = function(msg) {
-    $('.dropdown-menu').prepend('<li><a href="#">New message</a></li>')
-    }
-
-    load_msg = function() {
-    }
-
-    delete_msg = function(msg) {
-        $()
-    }
-
     // set elements behaviors
     $('.dropdown-menu').on('click.bs.dropdown', function(event) {
         event.stopPropagation();
         event.preventDefault();
-    })
+    });
+
     $('ul.dropdown-menu > li > button.close').on('click', function(event) {
         $(this).parent().remove();
-    })
+    });
 })
--- notifica/static/notifica/ws.js
+++ notifica/static/notifica/ws.js
@@ -1,7 +1,4 @@
 socket = new WebSocket("ws://" + window.location.host + "/");
-socket.onmessage = function(e) {
-    console.log(e.data);
-}
-socket.onclose = function(event) {
-    console.log('ws server down');
-}
+//socket.onclose = function(event) {
+//    console.log('WS server disconnected!');
+//}
