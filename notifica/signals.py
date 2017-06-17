76f5c780f2d56b89fb4866269b45ad765ddb7928
Harry Huy <harryhuydoan@gmail.com>
2017-06-18 01:30:59 +0700

    Remove: Websocket demultiplex replaced

--- notifica/signals.py
+++ notifica/signals.py
@@ -4,17 +4,4 @@ from django.dispatch import receiver
 from django.contrib.auth import user_logged_in, user_logged_out
 
 
-@receiver(post_save, sender=Notify)
-def new_notify(sender, **kargs):
-    if kargs['created'] == True:
-        message = {
-            'creator': kargs['instance'].creator.username,
-            'recipient': kargs['instance'].recipient.username,
-            'state': kargs['instance'].state,
-            'type': kargs['instance'].type,
-            'url': kargs['instance'].url
-        }
-        Channel('notify').send({'text': message})
-
-
 
