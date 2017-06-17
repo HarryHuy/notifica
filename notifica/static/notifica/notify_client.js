21ae283fe49510d290c63499df49fddd03cdd26e
Harry Huy <harryhuydoan@gmail.com>
2017-06-18 01:25:44 +0700

    Simplify: Websocket demultiplex implementing

--- notifica/static/notifica/notify_client.js
+++ notifica/static/notifica/notify_client.js
@@ -1,19 +1,3 @@
-socket.onmessage = function(event) {
-//    load_msg(event.text);
-    console.log(event.data);
-}
-
-//var load_msg = function(msg) {
-//    var type = msg['type'];
-//    var creator = msg['creator'];
-//    var url = msg['url'];
-//    $('.dropdown-menu').prepend('<li><a href="'+ url +'">'+ type + ', '+ creator +'</a></li>');
-//}
-//
-//var delete_msg = function(msg) {
-//    socket.send('delete'+ msg);
-//}
-
 $('document').ready(function() {
     // set elements behaviors
     $('.dropdown-menu').on('click.bs.dropdown', function(event) {
