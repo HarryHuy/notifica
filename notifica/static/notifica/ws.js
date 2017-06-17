e94d1884f42eb944612355f787c2dbad54a63126
Harry Huy <harryhuydoan@gmail.com>
2017-06-18 01:26:24 +0700

    Working: websocket demultiplex implementing

--- notifica/static/notifica/ws.js
+++ notifica/static/notifica/ws.js
@@ -1,4 +1,18 @@
-socket = new WebSocket("ws://" + window.location.host + "/");
-//socket.onclose = function(event) {
-//    console.log('WS server disconnected!');
-//}
+$('document').ready(function () {
+    var ws_path =  "/stream/";
+    console.log("Connecting to " + ws_path);
+
+    var webSocketBridge = new channels.WebSocketBridge();
+    webSocketBridge.connect(ws_path);
+    webSocketBridge.listen();
+    webSocketBridge.demultiplex('notify', function(payload, streamName) {
+        console.log(payload, streamName)
+    });
+    webSocketBridge.demultiplex('message', function(payload, streamName) {
+        console.log(payload, streamName)
+    })
+
+    // Helpful debugging
+    webSocketBridge.socket.addEventListener('open', function() { console.log("Connected to notification socket"); });
+    webSocketBridge.socket.addEventListener('close', function() { console.log("Disconnected to notification socket"); });
+});
