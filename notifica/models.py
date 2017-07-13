e3baab02673440a85dff1db080044c4f9956991a
Harry Huy <harryhuydoan@gmail.com>
2017-07-14 05:00:00 +0700

    Add: models, str method, org members

--- notifica/models.py
+++ notifica/models.py
@@ -21,9 +21,14 @@ class ExtendedUser(AbstractUser):
         return self.username
 
 
+
 class Org(models.Model):
     name = models.CharField(max_length=30)
     description = models.CharField(max_length=50)
+    member = models.ManyToManyField(ExtendedUser)
+
+    def __str__(self):
+        return self.name
 
 
 class Activity(models.Model):
@@ -32,13 +37,19 @@ class Activity(models.Model):
     date = models.DateField()
     participate = models.ManyToManyField(settings.AUTH_USER_MODEL)
 
+    def __str__(self):
+        return self.name
+
 
 class Notify(models.Model):
     recipient = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='notify_recipient')
     creator = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='notify_sender')
-    state = ('read', 'unread', 'unseen')
-    type = models.CharField(max_length=30)
+    state = ('read', 'unread')
+    content = models.CharField(max_length=30)
     url = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.content
 
 class Message(models.Model):
     recipient = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='message_recipient')
@@ -46,14 +57,21 @@ class Message(models.Model):
     state = ('read', 'unread', 'unseen')
     content = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.content
+
 class NotifyBinding(WebsocketBinding):
     model = Notify
     stream = 'notify'
-    fields = ['recipient', 'creator', 'state', 'type', 'url']
+    fields = ['recipient', 'creator', 'state', 'content', 'url']
 
+    # groups receiving data
     @classmethod
     def group_names(cls, *args, **kwargs):
-        return ['demultiplex']
+        group = list()
+        for notify in args:
+            group.append('ws-group-%s' % notify.recipient.id)
+        return group
 
     def has_permission(self, user, action, pk):
         return True
@@ -69,6 +87,7 @@ class MessageBinding(WebsocketBinding):
         for message in args:
             group.append('demultiplex-%s' % message.recipient.id)
         return group
+        return ['ws-group-%s' % message.recipient.id]
 
     def has_permission(self, user, action, pk):
         return True
