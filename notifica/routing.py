3b5e3c213f843bcbea269b788d1a9fe74871bd35
Harry Huy <harryhuydoan@gmail.com>
2017-07-14 05:04:11 +0700

    Edit: routing stream name

--- notifica/routing.py
+++ notifica/routing.py
@@ -1,5 +1,6 @@
-from channels.routing import route_class
+from channels.routing import route_class, route
 from .consumers import Demultiplexer
+from notifica import consumers
 
 channel_routing = [
     # route("http.request", "notifica.consumers.http_consumer"),
@@ -7,5 +8,5 @@ channel_routing = [
     # route('websocket.receive', consumers.ws_message),
     # route('websocket.disconnect', consumers.ws_disconnect),
     # route('notify', consumers.ws_send_notify),
-    route_class(Demultiplexer, path='^/stream/?$')
+    route_class(Demultiplexer, path='^/ws-stream/?$')
 ]
