e60b3be8372d66b641cc5c310f15c1808959eb60
Harry Huy <harryhuydoan@gmail.com>
2017-07-14 05:02:17 +0700

    Enable: signals

--- notifica/signals.py
+++ notifica/signals.py
@@ -1,7 +1,30 @@
-from django.db.models.signals import post_save
-from .models import Notify
+from django.db.models.signals import post_save, pre_save, m2m_changed
+from .models import Notify, ExtendedUser, Org
 from django.dispatch import receiver
 from django.contrib.auth import user_logged_in, user_logged_out
 
 
+# @receiver(post_save, sender=ExtendedUser)
+def user_changed(*args, **kwargs):
+    if not kwargs['created']:
+        notify = Notify()
+        try:
+            notify.creator = ExtendedUser.objects.get(id=kwargs['instance'].updated_by)
+            notify.recipient = ExtendedUser.objects.get(id=kwargs['instance'].id)
+        except ExtendedUser.DoesNotExist:
+            print("Notify's user error")
+        except AttributeError:
+            print('Wrong update')
+        else:
+            notify.state = 'unread'
+            notify.type = 'Your information has been changed by %s' % notify.creator
+            notify.url = 'none'
+            notify.save()
 
+@receiver(post_save, sender=Org)
+def org_changed(**kwargs):
+    print(kwargs)
+
+@receiver(m2m_changed, sender=Org.member.through)
+def org_m2m_changed(**kwargs):
+    print(kwargs)
