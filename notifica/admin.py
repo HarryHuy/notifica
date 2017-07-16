db17ff7ded6049e27dd5fce60f9d7e7407a52aa5
Harry Huy <harryhuydoan@gmail.com>
2017-07-17 00:15:21 +0700

    Edit: custom admin

--- notifica/admin.py
+++ notifica/admin.py
@@ -2,9 +2,19 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from notifica.models import *
 
-admin.site.register(ExtendedUser, UserAdmin)
 admin.site.register(Org)
 admin.site.register(Activity)
 admin.site.register(Notify)
 admin.site.register(Position)
 admin.site.register(Message)
+
+class MemberOrgInline(admin.TabularInline):
+    model = Org.member.through
+    extra = 1
+
+class ExtendedUserAdmin(admin.ModelAdmin):
+    inlines = [
+        MemberOrgInline,
+    ]
+
+admin.site.register(ExtendedUser, ExtendedUserAdmin)
