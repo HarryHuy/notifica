4d2314042fe9803c3c1f8c4d4941b5f780f6dc63
Harry Huy <harryhuydoan@gmail.com>
2017-07-17 19:09:29 +0700

    Git push: static files

--- notifica/static/notifica/ws.js
+++ notifica/static/notifica/ws.js
@@ -1,18 +1,18 @@
 $('document').ready(function () {
-    var ws_path =  "/stream/";
-    console.log("Connecting to " + ws_path);
-
-    var webSocketBridge = new channels.WebSocketBridge();
-    webSocketBridge.connect(ws_path);
-    webSocketBridge.listen();
-    webSocketBridge.demultiplex('notify', function(payload, streamName) {
-        console.log(payload, streamName)
-    });
-    webSocketBridge.demultiplex('message', function(payload, streamName) {
-        console.log(payload, streamName)
-    })
-
-    // Helpful debugging
-    webSocketBridge.socket.addEventListener('open', function() { console.log("Connected to notification socket"); });
-    webSocketBridge.socket.addEventListener('close', function() { console.log("Disconnected to notification socket"); });
+//    var ws_path =  "/ws-stream/";
+//    console.log("Connecting to " + ws_path);
+//
+//    var webSocketBridge = new channels.WebSocketBridge();
+//    webSocketBridge.connect(ws_path);
+//    webSocketBridge.listen();
+//    webSocketBridge.demultiplex('notify', function(payload, streamName) {
+//        console.log(payload, streamName)
+//    });
+//    webSocketBridge.demultiplex('message', function(payload, streamName) {
+//        console.log(payload, streamName)
+//    })
+//
+//    // Helpful debugging
+//    webSocketBridge.socket.addEventListener('open', function() { console.log("Connected to notification socket"); });
+//    webSocketBridge.socket.addEventListener('close', function() { console.log("Disconnected to notification socket"); });
 });
