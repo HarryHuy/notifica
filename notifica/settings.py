021f1403dd417d8da9437d37ef1c6b6307498357
Harry Huy <harryhuydoan@gmail.com>
2017-07-19 10:49:00 +0700

    Edit: dev settings with sqlite

--- notifica/settings.py
+++ notifica/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -25,7 +26,10 @@ SECRET_KEY = '0k#6wkhm*ui!nr^42z$oo+esbso_273a&&r+z*4c_pku2ha!$_'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'damp-springs-69076.herokuapp.com',
+    '127.0.0.1',
+]
 
 
 # Application definition
@@ -78,8 +82,9 @@ WSGI_APPLICATION = 'notifica.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         # 'ENGINE': 'django.db.backends.mysql',
+        # 'ENGINE': 'django.db.backends.postgresql',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         # 'NAME': 'test',
         # 'USER': 'test',
         # 'PASSWORD': '12345678',
@@ -124,10 +129,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    '/media/harry/data/python/notifica/notifica/static'
+    os.path.join(PROJECT_ROOT, 'static'),
 ]
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 CHANNEL_LAYERS = {
     "default": {
@@ -139,3 +147,6 @@ CHANNEL_LAYERS = {
 LOGIN_REDIRECT_URL = '/'
 
 AUTH_USER_MODEL = 'notifica.ExtendedUser'
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
