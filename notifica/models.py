d04120097d169c24860260b6f13fa995f797c76d
Harry Huy <harryhuydoan@gmail.com>
2017-09-10 15:09:38 +0700

    Add org field to ExtendedUser

--- notifica/models.py
+++ notifica/models.py
@@ -5,24 +5,25 @@ from django.conf import settings
 
 class Position(models.Model):
     name = models.CharField(max_length=30)
-    # permission = models.CharField(max_length=30)
-    description = models.CharField(max_length=50, null=True)
+    permission = models.CharField(max_length=30, blank=True)
+    description = models.CharField(max_length=50, blank=True)
 
     def __str__(self):
         return self.name
 
 
+class Organization(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.CharField(max_length=50, blank=True)
+
+
 class ExtendedUser(AbstractUser):
-    code = models.PositiveIntegerField(null=True)
-    position = models.ForeignKey(Position, null=True)
+    code = models.PositiveIntegerField(blank=True, null=True)
+    position = models.ManyToManyField(Position)
+    org = models.ManyToManyField(Organization)
 
     def __str__(self):
         return self.username
-
-
-class Organization(models.Model):
-    name = models.CharField(max_length=30)
-    description = models.CharField(max_length=50)
 
 
 class Activity(models.Model):
@@ -36,5 +37,5 @@ class Notify(models.Model):
     recipient = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='notify_recipient')
     creator = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='notify_sender')
     state = ('read', 'unread', 'unseen')
-    type = models.CharField(max_length=30)
-    url = models.CharField(max_length=50)
+    type = models.CharField(max_length=30, blank=True)
+    url = models.CharField(max_length=50, blank=True)
