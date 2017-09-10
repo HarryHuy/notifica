f910bf01d1165e10f78efce6df27bec538763176
Harry Huy <harryhuydoan@gmail.com>
2017-09-10 17:59:20 +0700

    Recieve m2m_change signal

--- notifica/signals.py
+++ notifica/signals.py
@@ -1,12 +1,12 @@
-from django.db.models.signals import post_save
-from .models import Notify
+from django.db.models.signals import post_save, m2m_changed
+from .models import *
 from django.dispatch import receiver
 from django.contrib.auth import user_logged_in, user_logged_out
 
 
 @receiver(post_save, sender=Notify)
 def new_notify(sender, **kargs):
-    if kargs['created'] == True:
+    if kargs['created'] is True:
         message = {
             'creator': kargs['instance'].creator.username,
             'recipient': kargs['instance'].recipient.username,
@@ -17,4 +17,14 @@ def new_notify(sender, **kargs):
         Channel('notify').send({'text': message})
 
 
+@receiver(m2m_changed, sender=ExtendedUser.org.through)
+def organization_changed(sender, **kwargs):
+    if kwargs['action'] in ('post_add', 'post_remove'):
+        instance = kwargs['instance']
+        notify = Notify.objects.create(
+            recipient=instance,
+            creator=ExtendedUser.objects.get(pk=instance['creator_id']),
+            state='unread',
+            type=kwargs['action'],
+        )
 
